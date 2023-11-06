4521445ee703f3392060bbf0fc04ca8664f74b21
jeff <jeff@granularit.com>
2023-11-06 16:17:05 +0300

    Trigger build

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,12 +15,12 @@ export default async function SetupLayout({
 
     const store = await prismadb.store.findFirst({
         where: {
-            userId
+            userId,
         }
     });
 
     if (store) {
-        redirect(`/${store.id}`);
+        redirect(`/${store?.id}`);
     }
 
     return (
