6a61249627b62b674e203b27da5c93efcea1a72d
jeff <jeff@granularit.com>
2023-11-08 14:40:23 +0300

    Add logs to access data on console for prod

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -24,6 +24,8 @@ export default function StoreSwitcher({
     const storeModel = useStoreModal();
     const params = useParams();
     const router = useRouter();
+    console.log("Params", params);
+    console.log("items", items);
 
     const formattedItems = items?.map((item) => ({
         label: item?.name,
