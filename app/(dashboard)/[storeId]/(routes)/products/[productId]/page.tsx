e5cbc83ae48b6936c19948681cb028b08c657c39
jeff <jeff@granularit.com>
2023-11-15 15:25:44 +0300

    Add models for products and product images

---
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,37 @@
+import prismadb from "@/lib/prismadb";
+
+import { BillboardForm } from "./components/billboard-form";
+import { generate12ByteId } from "@/lib/utils";
+
+const BillBoardPage = async ({
+    params
+}: { 
+    params: { billboardId: string }
+}) => {
+
+    // check billboard ID
+    const checkParamIdForBillboard = (params: any) => {
+        if (params?.billboardId === "new") {
+            const id = generate12ByteId(12);
+            return id;
+        } else {
+            return params?.billboardId;
+        }
+    }
+
+    const billboard = await prismadb.billboard.findUnique({
+        where: {
+            id: checkParamIdForBillboard(params)
+        }
+    });
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardForm initialData={billboard} />
+            </div>
+        </div>
+    )
+}
+
+export default BillBoardPage; 
